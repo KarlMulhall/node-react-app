81ef109d0052a6ba571eaf9cbb42e4048384dc77
Karl Mulhall <karlmulhall@live.ie>
2023-11-22 01:17:39 +0000

    Backend successfully fetches data from the REST Countries API. Next step is to parse the data and extract what is relevant for display.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({"countryInfo": ["name", "capital", "population", "currencies"] })
+app.get("/country/:name", (req, res) => {
+
+  try{
+    const {name} = req.params
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        res.send(data)
+      })
+      .catch(err => console.log('Error:', err.message));
+
+  }catch(e){
+    res.send(404).render('error', {message: "Country not found..."})
+  }
+  
 })
 
-
+app.get("/apitest", (req, res) => {
+    res.json({"countryInfo": ["name", "capital", "population", "currencies"] })
+})
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
